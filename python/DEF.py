7a9aef3efe261ef97de1f24e2fc13b7297b56847
TKSENN <taka23810@gmail.com>
2024-03-06 23:54:34 +0900

    Create DEF.py

---
+++ python/DEF.py
@@ -0,0 +1,18 @@
+def w(n):
+    for i in range(n):
+        print('w', end='')
+
+def x(n, c):
+    for i in range(n):
+        print(c, end='') 
+
+#n=5の場合  
+w(5)
+print('\n')
+
+#n=3の場合
+w(3)
+print('\n')
+        
+x(3, 'A')
+print('\n')
